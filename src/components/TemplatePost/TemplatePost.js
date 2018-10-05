e0c8657aabb11d6e8f588dea05133a5cb496da89
26thSpring <foelsk56@gmail.com>
2018-10-05 18:06:07 +0900

    템플릿 페이지 작업 진행 70프로

--- src/components/TemplatePost/TemplatePost.js
+++ src/components/TemplatePost/TemplatePost.js
@@ -8,7 +8,7 @@ import { IconContext } from 'react-icons';
 const TemplatePost = () => {
    return (
       <div className="TemplatePost">
-         <Link className="TemplatePost_head" to="/">
+         <Link className="TemplatePost_head" to="/template">
             <div className="TemplatePost_head_thumbnail">
                <IconContext.Provider value={{ size: '80', color: 'white' }}>
                   <MdImage />
@@ -18,23 +18,29 @@ const TemplatePost = () => {
          </Link>
          <div className="TemplatePost_body">
             <div className="TemplatePost_body_info">
-               <div className="writer">webber</div>
+               <div className="writer">
+                  <Link to="/template">webber</Link>
+               </div>
                <div className="regdate_reply">2 days ago · 3 reply</div>
             </div>
             <div className="TemplatePost_body_toggle">
-               <IconContext.Provider value={{ size: '23' }}>
-                  <div className="toggle_like">
+               <IconContext.Provider
+                  value={{ size: '23', className: 'likeIcon' }}
+               >
+                  <div className="toggle_like flex-row-center">
                      <TiHeartOutline />
                      13
                   </div>
                </IconContext.Provider>
-               <IconContext.Provider value={{ size: '23' }}>
-                  <div className="toggle_bookmark">
+               <IconContext.Provider
+                  value={{ size: '23', className: 'bookmarkIcon' }}
+               >
+                  <div className="toggle_bookmark flex-row-center">
                      <TiBookmark />
                   </div>
                </IconContext.Provider>
                <IconContext.Provider value={{ size: '23' }}>
-                  <div className="button_more">
+                  <div className="button_more flex-row-center">
                      <MdMoreVert />
                   </div>
                </IconContext.Provider>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,4 @@ import withSplitting from 'lib/withSplitting';
 
 export const Intro = withSplitting(() => import('./Intro'));
 export const Login = withSplitting(() => import('./Login'));
-<<<<<<< HEAD
 export const Template = withSplitting(() => import('./Template'));
-=======
-export const Community = withSplitting(() => import('./Community'));
->>>>>>> 7f77b402b2782a625735c41db288a0e89aadf83d
