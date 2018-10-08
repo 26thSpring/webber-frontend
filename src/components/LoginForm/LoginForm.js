3488d405f20f999561fc86c9e49ea5707581efc8
Jace <testblank@naver.com>
2018-10-08 11:45:51 +0900

    댓글창 수정

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './LoginForm.scss';
+import login_google from 'static/images/login_google.svg';
+import login_facebook from 'static/images/login_facebook.svg';
 
 const LoginForm = () => {
    return (
       <div className="LoginForm">
-         <div className="LoginHeader">login with</div>
+         <div className="LoginHeader">SignIn / SignUp with</div>
          <div className="LoginBody">
-            <div className="login_button_google">구글</div>
-            <div className="login_button_facebook">페이스북</div>
-            <div className="login_button_naver">네이버</div>
+            <div className="login_button_google">
+               <div className="Google">
+                  <img src={login_google} alt="login_google" />
+               </div>
+               <div className="Google">Google</div>
+            </div>
+            <div className="login_button_facebook">
+               <img src={login_facebook} alt="login_facebook" />
+               <div className="Facebook">FaceBook</div>
+            </div>
+            <div className="login_button_naver">
+               <img src="" alt="" />
+               <div className="Naver">Naver</div>
+            </div>
          </div>
       </div>
    );
--- src/components/Reply/Reply.js
+++ src/components/Reply/Reply.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Reply.scss';
+import reply_modify from 'static/images/reply_modify.svg';
+import reply_trash from 'static/images/reply_trash.svg';
 
 const Reply = () => {
    return (
@@ -10,12 +12,24 @@ const Reply = () => {
                alt="Reply_thumbnail"
             />
          </div>
-         <div className="Reply_head">
-            <div className="Reply_nickname">Reply_nickname</div>
-            <div className="Reply_regdate">2018-10-05</div>
-         </div>
          <div className="Reply_body">
+            <div className="Reply_head">
+               <div className="Reply_nickname">Reply_nickname</div>
+               <div className="Reply_regdate">2018-10-05</div>
+            </div>
             <div className="Reply_contents">this is Reply contents</div>
+            <div className="Reply_buttons">
+               <img
+                  className="Reply_modify"
+                  src={reply_modify}
+                  alt="Reply_modify"
+               />
+               <img
+                  className="Reply_trash"
+                  src={reply_trash}
+                  alt="Reply_trash"
+               />
+            </div>
          </div>
       </div>
    );
